e416bec1176aa89ca3eddaa73c3712f6e7cc9f6b
Julius Hell <jhell2306@googlemail.com>
2023-06-21 18:44:12 +0200

    Removed old news list

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,23 +33,4 @@
 		</div>
 		</a>
 	{/each}
-	<dl class="list-dl">
-		{#each data.news as newsItem}
-			<hr />
-			<div>
-				<a href="{newsItem.link}">
-				<span class="flex-auto">
-					<dt class="font-extrabold" style="white-space: normal">{newsItem.title}</dt>
-					<dd>{newsItem.feed} -
-						{#if newsItem.feed !== 'hn'}
-							{newsItem.creator}
-						{:else}
-							{new URL(newsItem.link).hostname}
-						{/if}
-					</dd>
-				</span>
-				</a>
-			</div>
-		{/each}
-	</dl>
 </div>
